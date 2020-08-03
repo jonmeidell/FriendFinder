aee01120dbda9c8172927ba2d882e6d2a743f16a
jonmeidell <jonmeidell@gmail.com>
2020-08-02 21:37:23 -0600

    completed more of survey functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,8 @@ const Express = require("express");
 const router = Express.Router();
 const data = require("../data/friends");
 
+module.exports = router
+
 router.get("/friends", (req, res) => {
     res.json(data)
 });
@@ -9,6 +11,46 @@ router.post("/friends", (req, res) => {
     console.log(req.body)
     data.push(req.body)
     res.json(data)
+    let returnSurvey = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: [
+            parseInt(req.body.question1),
+            parseInt(req.body.question2),
+            parseInt(req.body.question3),
+            parseInt(req.body.question4),
+            parseInt(req.body.question5),
+            parseInt(req.body.question6),
+            parseInt(req.body.question7),
+            parseInt(req.body.question8),
+            parseInt(req.body.question9),
+            parseInt(req.body.question10),
+        ]
+    }
+    const result = [];
+    for (let i = 0; i < friends.length; i++) {
+        const friend = friends[i];
+        const friendScores = friend.scores;
+        const differences = [];
+
+        for (let i = 0; i < friendScores.length; i++) {
+            const friendScore = friendScores[i];
+            differences.push(calc(returnSurvey.scores[i], friendScore));
+        }
+
+        var difference = differences.reduce((a, b) => a + b, 0);
+        result.push({
+            friend: friend,
+            difference: difference
+        });
+
+
+    }
+    friends.push(returnSurvey);
+    result.sort((a, b) => (a.difference > b.difference) ? 1 : -1);
+    return res.json(result[0]);
 });
 
-module.exports = router
+var calc = function (a, b) {
+    return Math.abs(parseInt(a) - parseInt(b));
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,4 +5,8 @@ router.get("/survey", (req, res) => {
     res.sendFile(__dirname, "../public/survey.html")
 });
 
+router.get("/*", (req, res) => {
+    res.sendFile(__dirname, "../public/index.html")
+});
+
 module.exports = router
